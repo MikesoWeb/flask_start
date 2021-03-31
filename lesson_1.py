089c20ac5c6fdfe1ed30c3f870f8680e48b00f86
MikesoWeb <klionpsyx@gmail.com>
2021-03-31 13:38:05 +0300

    'first_commit'

---
+++ lesson_1.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    name = 'Mike'
+    return f'<h1>Hello, {name}</h1>'
+
+
+if __name__ == '__main__':
+    app.run(debug=1, port=8000)
