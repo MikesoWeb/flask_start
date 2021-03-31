132d5c6b14d2acfa79011154f88efa5c51fc0095
MikesoWeb <klionpsyx@gmail.com>
2021-03-31 15:37:05 +0300

    add template & css

--- lesson_2.py
+++ lesson_2.py
@@ -1,16 +1,14 @@
- 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    name = 'Mike'
-    return f'<h1>Hello, {name}</h1>'
+    return render_template('index.html', name='Mike')
 
 
 if __name__ == '__main__':
     app.run(debug=1, port=8000)
 
-"Урок 2"
+
