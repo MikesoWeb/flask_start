d2744e0869f380a14a838f37886864dba8df2057
MikesoWeb <klionpsyx@gmail.com>
2021-03-31 14:35:36 +0300

    'change_code'

---
+++ lesson_2.py
@@ -0,0 +1,16 @@
+ 
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    name = 'Mike'
+    return f'<h1>Hello, {name}</h1>'
+
+
+if __name__ == '__main__':
+    app.run(debug=1, port=8000)
+
+"Урок 2"
